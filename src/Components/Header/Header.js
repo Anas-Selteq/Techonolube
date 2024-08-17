8db1da671c84b9eae760bf23fe0a4e70bad4a43c
Meeting Room <anas@selteq.net>
2024-08-17 15:29:22 +0500

    git push

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../Header/Header.css"
+
+function Header() {
+    return (
+        <div className='col-md-12 padding_left_right'>
+            <div className='col-md-12 background_cc py-3'>
+                <div className='col-md-6'>
+                    TAL(Technolube Automatic Lubrication)
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/Pages/Homepage/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../Components/Header/Header'
+
+function Home() {
+  return (
+    <div className='col-md-12'>
+      <Header />
+      Home
+    </div>
+  )
+}
+
+export default Home
