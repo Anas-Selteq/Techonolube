7592e83eff734281cde2f641e707e727f4fb9467
unknown <anas@selteq.net>
2024-08-21 00:01:39 +0500

    new chnages

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,9 +4,14 @@ import "../Header/Header.css"
 function Header() {
     return (
         <div className='col-md-12 padding_left_right'>
-            <div className='col-md-12 background_cc py-3'>
-                <div className='col-md-6'>
-                    TAL(Technolube Automatic Lubrication)
+            <div className='col-md-12 background_cc py-3 px-4'>
+                <div className='row'>
+                    <div className='col-md-6'>
+                        <img className='img-fluid' src='/images/logo.svg' />
+                    </div>
+                    <div className='col-md-6 text-end  m-auto'>
+                        <img className='img-fluid' src='/images/burger-m.svg' />
+                    </div>
                 </div>
             </div>
         </div>
--- src/Pages/Homepage/Home.js
+++ src/Pages/Homepage/Home.js
@@ -5,9 +5,96 @@ function Home() {
   return (
     <div className='col-md-12'>
       <Header />
-      Home
-    </div>
+      <div className='col-md-12 padding_left_right mt-5'>
+        <p className='font_for_p'>
+          Technolube Inc. is a Richmond BC based company, specializing in providing reliable and user friendly automatic lubricators with your choice of grease fill, and user-fillable oil units.  Our lubricators are also available with stock greases (EP0, EP1 and EP2 types) shipped next day or better.  Some food grade greases or specialty lubricant greases may take a little longer, but we will do our best to ship your lubricators containing the grease specified for your equipment in the shortest possible time, ready to go.
+        </p>
+        <p className='font_for_p'>
+          Electronically controlled lubrication has many advantages over the manual grease gun. Your lubricant is delivered in the precise amounts set up during installation of the lubricator.You enjoy better performance and longer machinery life (up to 48% longer), while reducing safety concerns and maintenance costs. The lubricator excludes air so there is less opportunity for hardening of grease in the bearing.
+        </p>
+        <p className='font_for_p'>
+          The units we supply start with the very user friendly single point Econo series and digital display D series, plus electromechanicals which can serve one or up to eight  bearings, explosion-proof versions for hazardous locations, user-fillable oil dispensers and the machine synchronized battery or external powered/remote control units.  For more  information please select the images below,  or use the model comparison/dispense rate chart button below to find the one(s) best suited to your equipment and ambience.
+        </p>
+        <p className='font_for_p'>
+          Grease brands in stock include Chevron, Dow, Lubriplate, Mobil, PetroCanada, Shell, Swepco and others.
+        </p>
+        <p className='font_for_p m-0 p-0'>
+          Currently our lubricators are being used in a variety of industries and locations including:
+        </p>
+        <div className='row'>
+          <div className='col-md-3'>
+            <p className='font_for_p m-0 p-0'>
+              Lumber Mills
+            </p>
+            <p className='font_for_p m-0 p-0'>
+              Paper Manufact
+            </p>
+            <p className='font_for_p m-0 p-0'>
+              Overhead Cranes
+            </p>
+            <p className='font_for_p m-0 p-0'>
+              Ski lifts
+            </p>
+            <p className='font_for_p m-0 p-0'>
+              Escalators...
+            </p>
+          </div>
+          <div className='col-md-3'>
+            <p className='font_for_p m-0 p-0'>
+              Lumber Mills
+            </p>
+            <p className='font_for_p m-0 p-0'>
+              Paper Manufact
+            </p>
+            <p className='font_for_p m-0 p-0'>
+              Overhead Cranes
+            </p>
+            <p className='font_for_p m-0 p-0'>
+              Ski lifts
+            </p>
+            <p className='font_for_p m-0 p-0'>
+              Escalators...
+            </p>
+          </div>
+          <div className='col-md-3'>
+            <p className='font_for_p m-0 p-0'>
+              Lumber Mills
+            </p>
+            <p className='font_for_p m-0 p-0'>
+              Paper Manufact
+            </p>
+            <p className='font_for_p m-0 p-0'>
+              Overhead Cranes
+            </p>
+            <p className='font_for_p m-0 p-0'>
+              Ski lifts
+            </p>
+            <p className='font_for_p m-0 p-0'>
+              Escalators...
+            </p>
+          </div>
+          <div className='col-md-3'>
+            <p className='font_for_p m-0 p-0'>
+              Lumber Mills
+            </p>
+            <p className='font_for_p m-0 p-0'>
+              Paper Manufact
+            </p>
+            <p className='font_for_p m-0 p-0'>
+              Overhead Cranes
+            </p>
+            <p className='font_for_p m-0 p-0'>
+              Ski lifts
+            </p>
+            <p className='font_for_p m-0 p-0'>
+              Escalators...
+            </p>
+          </div>
+        </div>
+        
+      </div>
+    </div >
   )
 }
 
-export default Home
+export default Home; 
